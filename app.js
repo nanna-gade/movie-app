131d732efb8bae548ff30dc697e3a47dd9fb78e5
nanna-gade <nanna.thgersen8@gmail.com>
2026-04-08 09:23:44 +0200

    Opgave 1.1-1.4

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-document.body.innerHTML += "Hello JavaScript!";
-// alert("Hello Dat.js");
-console.log("Hello Multimedia Designers");
-console.log("Hi Students!");
-console.log("This is your first JavaScript App 🎉");
+"use strict"; // Hjælper med at fange fejl
+
+console.log("Hej fra JavaScript! ");
+
+const message = "JavaScript er sejt!";
+console.log(message);
+
+const firstName = "Nanna";
+const age = 25;
+const isStudent = true;
+const favoriteMovie = "Find Nemo";
+
+console.log("Navn:", firstName);
+console.log("Alder:", age);
+console.log("Er studerende:", isStudent);
+console.log("Yndlingsfilm:", favoriteMovie);
