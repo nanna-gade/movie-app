c8b709a8b90bdd409e37da0cd5563891ca1aa96e
nanna-gade <nanna.thgersen8@gmail.com>
2026-04-08 09:28:26 +0200

    Opgave 2

--- app.js
+++ app.js
@@ -14,3 +14,22 @@ console.log("Navn:", firstName);
 console.log("Alder:", age);
 console.log("Er studerende:", isStudent);
 console.log("Yndlingsfilm:", favoriteMovie);
+
+let mood = "træt";
+console.log("Før:", mood);
+
+mood = "klar til JavaScript";
+console.log("Efter:", mood);
+
+// Find h1 elementet
+const heading = document.querySelector("h1");
+console.log("Jeg fandt h1:", heading);
+
+// Find count span
+const countDisplay = document.querySelector("#counter");
+console.log("Jeg fandt count:", countDisplay);
+
+// Find knapperne
+const clickButton = document.querySelector("#click-button");
+const resetButton = document.querySelector("#reset-button");
+console.log("Jeg fandt knapperne:", clickButton, resetButton);
