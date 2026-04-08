18bcf79331c3065b3a2e1a2846ee894b4eca6db8
nanna-gade <nanna.thgersen8@gmail.com>
2026-04-08 11:22:58 +0200

    Opgave 5+6

--- app.js
+++ app.js
@@ -1,49 +1,48 @@
-"use strict"; // Hjælper med at fange fejl
+"use strict";
 
-console.log("Hej fra JavaScript! ");
-
-const message = "JavaScript er sejt!";
-console.log(message);
-
-const firstName = "Nanna";
-const age = 25;
-const isStudent = true;
-const favoriteMovie = "Find Nemo";
-
-console.log("Navn:", firstName);
-console.log("Alder:", age);
-console.log("Er studerende:", isStudent);
-console.log("Yndlingsfilm:", favoriteMovie);
-
-let mood = "træt";
-console.log("Før:", mood);
-
-mood = "klar til JavaScript";
-console.log("Efter:", mood);
-
-// Find h1 elementet
-const heading = document.querySelector("h1");
-console.log("Jeg fandt h1:", heading);
-
-// Find count span
 const countDisplay = document.querySelector("#counter");
-console.log("Jeg fandt count:", countDisplay);
-
-// Find knapperne
 const clickButton = document.querySelector("#click-button");
 const resetButton = document.querySelector("#reset-button");
-console.log("Jeg fandt knapperne:", clickButton, resetButton);
 
-// Ændr overskriften
-heading.textContent = "Wow, jeg kan ændre tekst!";
+console.log(countDisplay);
+console.log(clickButton);
+console.log(resetButton);
 
-// Ændr count tallet
-countDisplay.textContent = "42";
 
-// Ændr tekstfarve
-heading.style.color = "yellow";
+const heading = document.querySelector("h1");
 
-// Ændr baggrund på count
-countDisplay.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
-countDisplay.style.padding = "10px";
-countDisplay.style.borderRadius = "10px";
+clickButton.addEventListener("click", function () {
+  console.log("Klik! ");
+  heading.textContent = "Du klikkede! ";
+});
+
+let count = 0;
+
+clickButton.addEventListener("click", function(){
+    count = count + 1;
+     countDisplay.textContent = count;
+       if (count > 40) {
+    countDisplay.style.color = "red";
+       }
+     if (count > 10) {
+    countDisplay.style.color = "lightgreen";
+  }
+     if(count === 12){
+        alert("du ramte 12 sejt");
+     };
+});
+
+resetButton.addEventListener("click", function(){
+    count = count = 0;
+    countDisplay.textContent = count;
+});
+
+
+const minusButton = document.querySelector("#minus-button");
+
+console.log(minusButton);
+
+minusButton.addEventListener("click", function () {
+  count = count - 1;
+  countDisplay.textContent = count;
+});
