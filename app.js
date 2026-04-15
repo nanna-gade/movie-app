7cdc2ff6f2a6b7a081090492a82b4ad23f1a3a30
nanna-gade <nanna.thgersen8@gmail.com>
2026-04-15 09:57:34 +0200

    Opgave 0.6-1.3

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log("Movie App - DAG 2")
+console.log("Movie App starter...");
+
+// Man kan også samle sit array/listen af film i et array, istedet for at lave EN variabel per film:
+const movies = ["Inception", "The Matrix", "Interstellar", "The Dark Knight", "Pulp Fiction"];
+
+console.log("Alle flm:", movies);
+
+console.log("Første film:", movies[0]);
+console.log("Anden film:", movies[1]);
+console.log("Sidste film:", movies[3]);
+
+console.log("Antal film:", movies.length);
