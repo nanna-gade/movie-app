fe582fcf0aa728fe8f1ae34d68ba5065446569fa
nanna-gade <nanna.thgersen8@gmail.com>
2026-04-15 11:25:07 +0200

    Opgave 4: Vis movie-cards med data

--- app.js
+++ app.js
@@ -1,47 +1,6 @@
 "use strict";
+console.log("Movie App starter...");
 
-// ALT DETTE ER FRA OPGAVE 2 (ARRAYS & LOOPS):
-// console.log("Movie App starter...");
-
-// // Man kan også samle sit array/listen af film i et array, istedet for at lave EN variabel per film:
-// const movies = ["Inception", "The Matrix", "Interstellar", "The Dark Knight", "Pulp Fiction", "Barbie", "The Notebook"];
-
-// console.log("Alle flm:", movies);
-
-// console.log("Første film:", movies[0]);
-// console.log("Anden film:", movies[1]);
-// console.log("Sidste film:", movies[3]);
-
-// console.log("Antal film:", movies.length);
-
-// const movieList = document.querySelector("#movie-list");
-// console.log(movieList);
-
-// for (const movie of movies) {
-//   const html = /* html */ `
-//     <article class="movie-card">
-//       <div class="movie-info">
-//         <h3>${movie}</h3>
-//       </div>
-//     </article>
-//   `;
-
-//   movieList.insertAdjacentHTML("beforeend", html);
-// }
-
-// KODE FRA OPGAVE 3.1:
-// const movie = {
-//   title: "Inception",
-//   year: 2010,
-//   rating: 8.8,
-// };
-
-// console.log("Titel:", movie.title);
-// console.log("År:", movie.year);
-// console.log("Rating:", movie.rating);
-
-
-// Et array af objects
 const movies = [
   {
     title: "Inception",
@@ -53,14 +12,33 @@ const movies = [
     year: 1999,
     rating: 8.7,
   },
+  {
+    title: "Interstellar",
+    year: 2014,
+    rating: 8.6,
+  },
+  {
+    title: "The Dark Knight",
+    year: 2008,
+    rating: 9.0,
+  },
 ];
 
-console.log("Første film:", movies [0]);
-console.log("Anden film:", movies[1]);
+console.log("Alle film:", movies);
 
-// Loop gennem arrayet af objects
+const movieList = document.querySelector("#movie-list");
+console.log(movieList);
+
 for (const movie of movies) {
-  console.log("Titel:", movie.title);
+  const html = /* html */ `
+    <article class="movie-card">
+      <div class="movie-info">
+        <h3>${movie.title}</h3>
+        <p>År: ${movie.year}</p>
+        <p>Rating: ${movie.rating}</p>
+      </div>
+    </article>
+  `;
+
+  movieList.insertAdjacentHTML("beforeend", html);
 }
-
-
