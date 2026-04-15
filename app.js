d13f35c324c81d08eeeb0ba8d912140586ccd546
nanna-gade <nanna.thgersen8@gmail.com>
2026-04-15 14:02:12 +0200

    Opgave 6

--- app.js
+++ app.js
@@ -7,22 +7,26 @@ const movies = [
     title: "Inception",
     year: 2010,
     rating: 8.8,
+    image: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg",
   },
   {
     title: "The Matrix",
     year: 1999,
     rating: 8.7,
+    image: "https://m.media-amazon.com/images/M/MV5BN2NmN2VhMTQtMDNiOS00NDlhLTliMjgtODE2ZTY0ODQyNDRhXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
   },
   {
     title: "Interstellar",
     year: 2014,
     rating: 8.6,
+    image: "https://m.media-amazon.com/images/I/91vIHsL-zjL._AC_UF894,1000_QL80_.jpg",
   },
   {
     title: "The Dark Knight",
     year: 2008,
     rating: 9.0,
-  },
+    image: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_FMjpg_UX1000_.jpg",
+  }, 
 ];
 
 const movieList = document.querySelector("#movie-list");
@@ -38,8 +42,11 @@ function showMovies() {
 }
 
 function showMovie(movie) {
+  const highlightClass = movie.rating > 8.5 ? "movie-card--highlight" : "";
+
   const html = /* html */ `
-    <article class="movie-card">
+    <article class="movie-card ${highlightClass}">
+      <img class="movie-image" src="${movie.image}" alt="${movie.title}">
       <div class="movie-info">
         <h3>${movie.title}</h3>
         <p>År: ${movie.year}</p>
@@ -54,7 +61,8 @@ function showMovie(movie) {
 movies.push({
   title: "Pulp Fiction",
   year: 1994,
-  rating: 8.9,
+  rating: 8.2,
+  image: "https://i-viaplay-com.akamaized.net/viaplay-prod/771/672/1473257890-66ec43721fe0fd0073af100473a09da74924816c.jpg?width=400&height=600",
 });
 
 showMovies();
