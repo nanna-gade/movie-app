9029e608d5d1e9509757e95967446afba2442209
nanna-gade <nanna.thgersen8@gmail.com>
2026-04-15 10:53:54 +0200

    Opgave 2.1-2.5

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 console.log("Movie App starter...");
 
 // Man kan også samle sit array/listen af film i et array, istedet for at lave EN variabel per film:
-const movies = ["Inception", "The Matrix", "Interstellar", "The Dark Knight", "Pulp Fiction"];
+const movies = ["Inception", "The Matrix", "Interstellar", "The Dark Knight", "Pulp Fiction", "Barbie", "The Notebook"];
 
 console.log("Alle flm:", movies);
 
@@ -12,3 +12,21 @@ console.log("Anden film:", movies[1]);
 console.log("Sidste film:", movies[3]);
 
 console.log("Antal film:", movies.length);
+
+const movieList = document.querySelector("#movie-list");
+console.log(movieList);
+
+for (const movie of movies) {
+  const html = /* html */ `
+    <article class="movie-card">
+      <div class="movie-info">
+        <h3>${movie}</h3>
+      </div>
+    </article>
+  `;
+
+  movieList.insertAdjacentHTML("beforeend", html);
+}
+
+
+
